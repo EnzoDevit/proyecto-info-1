723557ff5aefb315bb5b8c9b47590a939188408f
EnzoDevit <enzodevit12@gmail.com>
2024-11-15 23:19:08 -0300

    funciones socket y archivos mainCliente y mainServer, librerias sock-lib.c y sock-lib.h, funciones y bucle infinito en mainServer.c

--- code/src/server_funcs.h
+++ code/src/server_funcs.h
@@ -1,4 +1,8 @@
 #include "BN.h"
+#include <stdio.h>//agregada
+#include <signal.h>//agregada
+#include <pthread.h>//agregada
+#include "sock-lib.h"//agregada
 
 int BN_checkship(struct BN_Board *, unsigned char x, unsigned char y, unsigned char dir);
 int BN_checkAllShipsDown(struct BN_Board *);
